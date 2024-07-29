5ccd579dabcc004612fb4fbb9f5b652471847a49
LUIZ ANTONIO MADRUGA <madruga_emo@hotmail.com>
2024-07-29 19:55:39 -0300

    Configurando a conexao

---
+++ AgroSoft/src/main/java/com/TCC/AgroSoft/AgroSoftApplication.java
@@ -0,0 +1,13 @@
+package com.TCC.AgroSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroSoftApplication.class, args);
+	}
+
+}
---
+++ AgroSoft/src/main/java/com/TCC/AgroSoft/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.TCC.AgroSoft.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+
+
+}
